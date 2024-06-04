7a50ede1585946528dc401b9f6e231bbff863140
Bruno Coelho <bruno-coelho@hotmail.com>
2024-06-03 21:43:22 -0300

    create class buscadorMoedas

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BuscadorMoeda moeda = new BuscadorMoeda();;
+        System.out.println("****************************");
+        System.out.println("\n Olá! Seja bem vindo ao seu conversor de moedas.");
+        System.out.println("\n****************************");
+
+        while (true) {
+            Scanner opcao = new Scanner();
+            System.out.println("Digite o valor que você deseja converter: ");
+            double valor = opcao.nextDouble();
+
+        }
+        }
+    }

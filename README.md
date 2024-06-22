65c86c7490301b47e5dd5c12d2b47577d09e53d1
Bruno Coelho <bruno-coelho@hotmail.com>
2024-06-22 20:20:12 -0300

    Finalização do desafio 🔚

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# conversorDeMoedasJava
-Criação de um conversor de Moedas em JAVA, com o consumo de uma API.
+# Conversor de Moedas 🪙
+
+Este é um projeto de um conversor de moedas desenvolvido em Java, utilizando a API de taxa de câmbio [ExchangeRate-API](https://www.exchangerate-api.com/). O projeto foi desenvolvido no IntelliJ IDEA. E é o desafio de projeto da formação Java do Alura.
+Mais informações você pode encontrar [aqui](https://cursos.alura.com.br/formacao-java) .
+
+## Descrição 📝
+
+O conversor de moedas permite que os usuários convertam valores entre diferentes moedas. As taxas de câmbio são obtidas em tempo real usando a API do ExchangeRate-API.
+
+## Funcionalidades ⚙️
+
+- Conversão de Euro (EUR) para Dólar (USD)
+- Conversão de Dólar (USD) para Euro (EUR)
+- Conversão de Real (BRL) para Dólar (USD)
+- Conversão de Dólar (USD) para Real (BRL)
+- Conversão de Pesos Argentinos (ARS) para Dólar (USD)
+- Conversão de Dólar (USD) para Pesos Argentinos (ARS)
+
+## Estrutura do Projeto 
+
+O projeto está dividido em duas classes principais:
+
+1. `Main`: Contém o método `main` e a lógica de interação com o usuário.
+2. `BuscadorMoeda`: Contém o método `converterMoeda` que realiza a conversão de moedas usando a API.
+
+## Como Usar
+
+1. Clone este repositório:
+    ```bash
+    git clone https://github.com/brunucoelho/conversor-moedas.git
+    ```
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd conversor-moedas
+    ```
+3. Abra o projeto no IntelliJ IDEA.
+4. Execute a classe `Main` para iniciar o conversor de moedas.
+5. Siga as instruções no menu para selecionar a conversão desejada e insira o valor a ser convertido.
+
+<h3 style="color: red;"> OBSERVAÇÕES </h3>
+Caso tenha intesse no folk do projeto, lembre-se de criar uma conta no [ExchangeRate-API](https://www.exchangerate-api.com/), para ter acesso ao Tolken da API.
+
+## Ferramentas Utilizadas
+
+- Java
+- IntelliJ IDEA
+- ExchangeRate-API
+- Biblioteca GSON para manipulação de JSON
+
+## Autor 👨🏽‍💻
+
+Desenvolvido por [brunucoelho](https://github.com/brunucoelho).
+
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,64 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        BuscadorMoeda moeda = new BuscadorMoeda();;
-        System.out.println("****************************");
-        System.out.println("\n Olá! Seja bem vindo ao seu conversor de moedas.");
-        System.out.println("\n****************************");
+        Scanner menu = new Scanner(System.in);
 
         while (true) {
-            Scanner opcao = new Scanner();
-            System.out.println("Digite o valor que você deseja converter: ");
-            double valor = opcao.nextDouble();
+            System.out.print(" **************************************************** \n");
+            //System.out.println("\n");
+            System.out.print(" -----  Seja bem vindo ao conversor de moedas (: ---- \n");
+            System.out.print(" 1) Dólar (USD)  ->  Pesos Argentinos(ARS)            \n");
+            System.out.print(" 2) Pesos Argentinos(ARS)  ->  Dólar (USD)            \n");
+            System.out.print(" 3) Dólar (USD)  ->  Real (BRL)                       \n");
+            System.out.print(" 4) Real (BRL)  ->  Dólar (USD)                       \n");
+            System.out.print(" 5) Dólar (USD)  ->  Peso colombiano (COP)            \n");
+            System.out.print(" 6) Peso colombiano (COP)  ->  Dólar (USD)            \n");
+            System.out.print(" 7) Sair                                              \n");
+            System.out.print(" **************************************************** \n");
+            //System.out.println("\n");
+            System.out.print("Escolha uma opção válida: \n");
 
-        }
+            int opcao = menu.nextInt();
+
+            if (opcao == 0) {
+                System.out.print("\nConversor Finalizado!");
+                menu.close();
+                break;
+            }
+
+            System.out.print("Digite o valor que deseja converter: \n");
+            double valor = menu.nextDouble();
+
+            switch (opcao) {
+                case 1:
+                    System.out.printf("\nValor de %.2f [USD] corresponde ao valor final de -> %.2f [ARS]\n", valor, BuscadorMoeda.converterMoeda("USD", "ARS", valor));
+                    break;
+
+                case 2:
+                    System.out.printf("\nValor de %.2f [ARS] corresponde ao valor final de -> %.2f [USD]\n", valor, BuscadorMoeda.converterMoeda("ARS", "USD", valor));
+                    break;
+
+                case 3:
+                    System.out.printf("\nValor de %.2f [USD] corresponde ao valor final de -> %.2f [BRL]\n", valor, BuscadorMoeda.converterMoeda("USD", "BRL", valor));
+                    break;
+
+                case 4:
+                    System.out.printf("\nValor de %.2f [BRL] corresponde ao valor final de -> %.2f [USD]\n", valor, BuscadorMoeda.converterMoeda("BRL", "USD", valor));
+                    break;
+
+                case 5:
+                    System.out.printf("\nValor de %.2f [USD] corresponde ao valor final de -> %.2f [COP]\n", valor, BuscadorMoeda.converterMoeda("USD", "COP", valor));
+                    break;
+
+                case 6:
+                    System.out.printf("\nValor de %.2f [COP] corresponde ao valor final de -> %.2f [USD]\n", valor, BuscadorMoeda.converterMoeda("COP", "USD", valor));
+                    break;
+
+                default:
+                    System.out.print("\nEssa conversão não é permitida, favor digitar um número válido!\n");
+                    break;
+            }
         }
     }
+}
